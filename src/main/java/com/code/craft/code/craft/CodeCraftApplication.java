c31564026855ad4d8e1f86cb8a263be1e479a8fb
aman prakash <amanjhasahab@gmail.com>
2023-09-06 13:52:55 +0530

    First Commit of whole application

---
+++ src/main/java/com/code/craft/code/craft/CodeCraftApplication.java
@@ -0,0 +1,13 @@
+package com.code.craft.code.craft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCraftApplication.class, args);
+	}
+
+}
